3c8c48dda5db6e1c41adaef6e897f632178e419f
JavierLi <missionbit@Missions-MacBook-Pro-7.local>
2017-04-06 17:54:53 -0700

    First Sprite Sheet Animation
    Sprite Sheet is Walkingss.png, Why Am I Writing Like This

--- base.js
+++ base.js
@@ -2,14 +2,14 @@ var game = new Phaser.Game(800, 600, Phaser.AUTO, 'game', { preload: preload, cr
 
 function preload() {
     
-    game.load.spritesheet('guy', 'assets/walkingss.png', 460.2, 590, 4);
+    game.load.spritesheet('guy', 'assets/walkingss.png', 281.5, 346, 8);
     game.load.image('background', 'assets/white.png')
 
 }
 
 function create() {
     
-              game.add.tileSprite(0, 0, 800, 600, 'background');
+        game.add.tileSprite(0, 0, 800, 600, 'background');
 
         var sprite = game.add.sprite(0, 0, 'guy');
         sprite.animations.add('walk');
